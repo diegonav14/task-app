5c56d7841c9f50a2175b6ad0c0ddc3dacb66f1ad
Diego <diego.navarro14@outlook.cl>
2023-01-24 17:32:18 -0300

    Buscar Tarea y conexion bd

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 $(function(){
 
-    console.log('Jquery esta funcionando')
+    console.log('Jquery esta funcionando');
+
+    $('#search').keyup(function() {
+        let search = $('#search').val();
+         $.ajax({
+            url: 'task-search.php',
+            type: 'POST',
+            data: {search},
+            success: function(response){
+                console.log(response)
+            }
+         });
+    });
 
 });
--- database.php
+++ database.php
@@ -2,11 +2,11 @@
 $connection = mysqli_connect(
     'localhost',
     'root',
-    'password',
+    '',
     'task-app'
 );
 
-    if($connection){
-        echo "Database is connected";
-    }
+    // if($connection){
+    //     echo "Database is connected";
+    // }
 ?>
